0cd9a3bd082d8ea5d74adeeb831dc1257a7a8196
jcloud <cloudsprogramming@gmail.com>
2025-02-28 18:11:06 -0800

    feature(route): added route for frontend to communicate

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express, { Request, Response } from 'express';
-import cookieParser from 'cookie-parser';
+import express, { Request, Response } from "express";
+import cookieParser from "cookie-parser";
 import config from "./config/config";
-import morgan from 'morgan'; // Http request logger, help debug
-import cors from 'cors';
-import { requiresAuthentication } from './middleware/authMiddleware';
+import morgan from "morgan"; // Http request logger, help debug
+import cors from "cors";
+import { requiresAuthentication } from "./middleware/authMiddleware";
 
 // Import routes
 import authRoutes from "./routes/auth";
@@ -17,9 +17,9 @@ const PORT: number = config.PORT;
 // Allows us to set new property `user` for Request object throughout this project
 // Ex: req.user will have info from users table, NOT from tenants table.
 declare module "express-serve-static-core" {
-    interface Request {
-        user?: any;
-    }
+  interface Request {
+    user?: any;
+  }
 }
 
 app.use(express.json());
@@ -27,13 +27,13 @@ app.use(morgan("common"));
 app.use(cookieParser()); // Enables reading cookies from req.cookies
 
 // Allows request from frontend
-app.use(cors({origin: "http://localhost:5173"}));
+app.use(cors({ origin: "http://localhost:5173" }));
 
 // Use routes
-app.use('/auth', authRoutes);
-app.use('/leases', requiresAuthentication, leaseRoutes);
+app.use("/auth", authRoutes);
+app.use("/leases", requiresAuthentication, leaseRoutes);
 
 // Listener
 app.listen(PORT, HOST, () => {
-    console.log(`Elite Space App is listening on port ${PORT} of ${HOST}!`);
-})
+  console.log(`Elite Space App is listening on port ${PORT} of ${HOST}!`);
+});
