70056931bb98c06bb107dc446fa5f97c0836d0a3
FELIPE SANCHEZ <felipesanchez10@protonmail.com>
2025-03-04 03:50:11 -0500

    fix(cors): added cors to allow frontend to connect

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
-import express, { application } from 'express';
+import express, { application } from "express";
 import config from "./config/config";
-import morgan from 'morgan'; // Http request logger, help debug
+import morgan from "morgan"; // Http request logger, help debug
 
 // Import routes
 import authRoutes from "./routes/auth";
-import miscRoutes from "./routes/miscRoutes"
+import miscRoutes from "./routes/miscRoutes";
 
+import cors from "cors";
 // Configuration
 const app = express();
 const HOST: string = config.HOST;
@@ -13,11 +14,13 @@ const PORT: number = config.PORT;
 
 app.use(morgan("common"));
 
+//Allows frontend to communicate with backend
+app.use(cors({ origin: "http://localhost:5173" }));
 // Use routes
 app.use(authRoutes);
-app.use('/api',miscRoutes);
+app.use("/api", miscRoutes);
 
 // Listener
 app.listen(PORT, HOST, () => {
-    console.log(`Elite Space App is listening on port ${PORT} of ${HOST}!`);
-})
+  console.log(`Elite Space App is listening on port ${PORT} of ${HOST}!`);
+});
