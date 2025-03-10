111cf1ff252b051b318e46bad4f7aa2bddc090ca
CloudsProgram <62721390+CloudsProgram@users.noreply.github.com>
2025-03-10 11:39:05 -0700

    feature(back-front-end-connection) established CORS which allows front-end request (#13)

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { application } from 'express';
 import config from "./config/config";
 import morgan from 'morgan'; // Http request logger, help debug
+import cors from 'cors';
 
 // Import routes
 import authRoutes from "./routes/auth";
@@ -12,6 +13,9 @@ const PORT: number = config.PORT;
 
 app.use(express.json());
 app.use(morgan("common"));
+
+// Allows request from frontend
+app.use(cors({origin: "http://localhost:5173"}));
 
 // Use routes
 app.use('/auth', authRoutes);
