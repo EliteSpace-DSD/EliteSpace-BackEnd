6952ff23d4aa6548446c3ceacbe43b5f93f09bb2
jcloud <cloudsprogramming@gmail.com>
2025-03-17 23:05:44 -0700

    feature(smart package): establish packages response for the front end

--- src/routes/smartPackage.ts
+++ src/routes/smartPackage.ts
@@ -4,11 +4,34 @@ import { getPackagesByTenantId } from '../db/models/packages'
 import { getTenantInfoByUserId } from '../db/models/tenant'
 const router = express.Router();
 
+export interface Package {
+    id: string;
+    deliveredDateTime: Date | null;
+    status: 'delivered' | 'retrieved';
+}
+
+// PackagesResponse is now an array of Package objects
+export type PackagesResponse = Package[];
+
 router.get('/', async (req: Request, res: Response) => {
         try {
-            const result = await getPackagesByTenantId(req.tenant.id);
-            console.log(result);
-            res.send("CHECK POINT 1, non-functional smartPackage endpoint");
+            const packagesArr = await getPackagesByTenantId(req.tenant.id);
+            if (!packagesArr) {
+                console.error("Unable to properly retrieve packages info");
+                return;
+            }
+            //Front end needs to check the length of the array, if EQUAL 0, then show
+            // UI that reflects that
+
+            const responseData: PackagesResponse = packagesArr.map(individualpackage => {
+                return {
+                    id: individualpackage.id,
+                    deliveredDateTime: individualpackage.deliveryTime, 
+                    status: individualpackage.status as 'delivered' | 'retrieved'
+                };
+            });
+
+            res.status(200).json(responseData);
         } catch (error) {
             res.status(500).json({ message: 'Server error' });
             return;
