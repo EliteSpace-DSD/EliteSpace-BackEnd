d4af196bc4f682fca0cae68c36e3edbf4500bbee
jcloud <cloudsprogramming@gmail.com>
2025-03-24 18:43:13 -0700

    fix(server) add auth check middleware to protected routes

--- src/server.ts
+++ src/server.ts
@@ -45,8 +45,8 @@ app.use(cors({
 app.use("/auth", authRoutes);
 app.use("/leases", requiresAuthentication, leaseRoutes);
 app.use("/parking", requiresAuthentication, parkingRoutes);
-app.use("/complaints", complaintRoutes);
-app.use("/accessCodes", accessCodesRoutes);
+app.use("/complaints", requiresAuthentication, complaintRoutes);
+app.use("/accessCodes", requiresAuthentication, accessCodesRoutes);
 app.use('/smartpackage', requiresAuthentication, smartPackageRoutes);
 app.use("/demo", demoRoutes);
 
