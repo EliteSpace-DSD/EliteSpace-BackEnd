c6507f254f1ae09ba73a21f7bc0bd97df65d1be1
CloudsProgram <62721390+CloudsProgram@users.noreply.github.com>
2025-03-24 22:27:43 -0700

    Console log debug 1 (#44)
    * refactor(complaints): remove console.log debug for resposne
    
    * debug(auth-email-confirmation): add console logs

--- src/routes/complaints.ts
+++ src/routes/complaints.ts
@@ -14,13 +14,12 @@ router.post("/submit-complaint", async (req: Request, res: Response) => {
     };
     console.log("Complaint Processed:", fullComplaint);
     //Call AI here
-    const check = res.status(200).json({
+    res.status(200).json({
       message: "Complaint submitted in successfully",
       complaint: fullComplaint,
       statusCode: 200
     });
 
-    console.log(check);
   } catch (error) {
     console.error("Error processing complaint:", error);
     res.status(500).json({ message: "Server error" });
