e393f7bdf4696dc6b3b1a7b1ee4327d67fd7e48e
jcloud <cloudsprogramming@gmail.com>
2025-03-27 11:53:37 -0700

    debug(auth): adjust console log

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -197,7 +197,6 @@ router.get("/verify-user", async (req: Request, res: Response) => {
 
     const tenantInfo = await getTenantInfoByUserId(data.user.id);
     if (!tenantInfo) {
-      console.log("Debug log 4:(tenantInfo) ", tenantInfo);
       res.status(401).json({ message: "Unable to find tenant in query." });
       return;
     }
