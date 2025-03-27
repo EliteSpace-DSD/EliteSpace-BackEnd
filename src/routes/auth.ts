ec7c9f6f4a5f611abc98b66c01e94eedcee46ca2
jcloud <cloudsprogramming@gmail.com>
2025-03-27 13:39:56 -0700

    debug(sign up): add console log

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -44,6 +44,12 @@ router.post("/register", async (req: Request, res: Response) => {
       return;
     }
 
+    console.log("Debug 1 (data)", data);
+    console.log("Debug 2 (data.user)", data.user);
+    if (data.user) {
+      console.log("Debug 3 (data.user.id)", data.user.id);
+    }
+    
     if (data.user) {
       const { error: dbError } = await linkUserToTenant(
         email,
